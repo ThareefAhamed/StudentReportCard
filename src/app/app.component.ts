75afd919349314bb20708c5f6ca49a4348737d17
ThareefAhamed <137420741+ThareefAhamed@users.noreply.github.com>
2024-07-28 13:00:11 +0530

    Student Report Card... Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  marks: number[][] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]];
+  total: number[] = [0, 0, 0, 0, 0];
+  grade: string[] = ['', '', '', '', '', ''];
   title = 'StudentReportCard';
+  isValid(e: any, i: number, j: number) {
+    if (e.target.value <= 100 && e.target.value >= 0) {
+      this.marks[i][j] = parseInt(e.target.value);
+      this.calculate();
+    } else {
+      alert("Please enter a valid marks");
+      e.target.value = 0;
+    }
+  }
+
+  calculate() {
+    this.total = [0, 0, 0, 0, 0];
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        this.total[i] += this.marks[i][j];
+      }
+    }
+  }
+
+  reset() {
+    this.marks = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0]];
+    this.grade = ['', '', '', '', ''];
+    this.total = [0, 0, 0, 0, 0];
+  }
+
+  findGrade() {
+    for (let i = 0; i < 5; i++) {
+      let avg = this.total[i] / 5;
+      if (avg <= 100 && avg >= 80) {
+        this.grade[i] = 'A++';
+      } else if (avg < 80 && avg >= 60) {
+        this.grade[i] = 'A';
+      } else if (avg < 60 && avg >= 50) {
+        this.grade[i] = 'B';
+      } else {
+        this.grade[i] = 'C';
+      }
+    }
+  }
 }
